a859b0b2b772f98d503f2b21d326c831881de679
dariokarin <dario.karin95@gmail.com>
2022-04-10 14:00:24 +0200

    sql file
    dodan sql file za kreiranje lokalne baze

--- CheapestFlightSearch/CheapestFlightSearch/CreateDB.sql
+++ CheapestFlightSearch/CheapestFlightSearch/CreateDB.sql
@@ -1,7 +1,7 @@
 USE [CheapestFlightsDB]
 GO
 
-/****** Object:  Table [dbo].[Airports]    Script Date: 7.4.2022. 11:27:33 ******/
+/****** Object:  Table [dbo].[Airports]    Script Date: 10.4.2022. 13:58:48 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -9,15 +9,29 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE TABLE [dbo].[Airports](
-	[ID] [int] IDENTITY(1,1) NOT NULL,
-	[IATA] [nchar](10) NULL,
-	[ICAO] [nchar](10) NULL,
-	[AirportName] [nvarchar](50) NULL,
-	[AirportLocation] [nvarchar](50) NULL,
- CONSTRAINT [PK_Airports] PRIMARY KEY CLUSTERED 
-(
-	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	[IATA] [nvarchar](50) NULL,
+	[ICAO] [nvarchar](50) NULL,
+	[AirportName] [nvarchar](500) NULL,
+	[AirportLocation] [nvarchar](500) NULL
 ) ON [PRIMARY]
 GO
 
+
+USE [CheapestFlightsDB]
+GO
+
+/****** Object:  Table [dbo].[SearchingHistory]    Script Date: 10.4.2022. 13:58:33 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[SearchingHistory](
+	[DepartureAirport] [nvarchar](50) NULL,
+	[ArrivalAirport] [nvarchar](50) NULL,
+	[DepartureDate] [datetime] NOT NULL,
+	[ReturnDate] [datetime] NOT NULL,
+	[Currency] [int] NOT NULL
+) ON [PRIMARY]
+GO
